6d2cfff53cc53c6d97ec9766d941f8fc8b74fba8
Kristián Kunc <kristian.kunc@gmail.com>
2024-09-24 17:01:09 +0200

    create tower helper

--- Assets/Scripts/Towers/Archer.cs
+++ Assets/Scripts/Towers/Archer.cs
@@ -16,10 +16,9 @@ public class Archer : MonoBehaviour
         //DrawRangeOutline();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        GameObject[] enemies = GetEnemiesInRange();
+        GameObject[] enemies = TowerHelpers.GetEnemiesInRange(transform, range);
         Debug.Log("Enemies in range: " + enemies.Length);
         Debug.Log("Can shoot: " + canShoot);
         if (enemies.Length > 0 && canShoot)
@@ -39,7 +38,6 @@ public class Archer : MonoBehaviour
         canShoot = true;
     }
 
-    // 100% Copilot, 0% muj kod
     IEnumerator AnimateArrow(GameObject enemy)
     {
         GameObject arrow = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
@@ -47,41 +45,12 @@ public class Archer : MonoBehaviour
         arrow.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
         arrow.GetComponent<Renderer>().material.color = Color.red;
 
-        Vector3 startPosition = transform.position;
-        Vector3 targetPosition = enemy.transform.position;
-        Vector3 controlPoint = (startPosition + targetPosition) / 2 + Vector3.up * 2;
-
-        float duration = 1f;
-        float startTime = Time.time;
-
-        while (Time.time < startTime + duration)
-        {
-            if (enemy == null)
-            {
-                // TODO: do not destroy arrows mid air
-                Destroy(arrow);
-                yield break;
-            }
-
-            targetPosition = enemy.transform.position;
-            controlPoint = (startPosition + targetPosition) / 2 + Vector3.up * 2;
-
-            float t = (Time.time - startTime) / duration;
-            Vector3 currentPosition = CalculateBezierPoint(t, startPosition, controlPoint, targetPosition);
-            arrow.transform.position = currentPosition;
-
-            Vector3 direction = (CalculateBezierPoint(t + 0.01f, startPosition, controlPoint, targetPosition) - currentPosition).normalized;
-            arrow.transform.rotation = Quaternion.LookRotation(direction) * Quaternion.Euler(90, 0, 0);
-
-            yield return null;
-        }
-
-        Destroy(arrow);
+        yield return TowerHelpers.AnimateBezierProjectile(arrow, transform.position, enemy, 2, 1, TowerHelpers.TowerProjectileRotationTypes.LOOK_AT_TARGET);
     }
 
-    Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    public static void KillArrow(GameObject arrow)
     {
-        return Mathf.Pow(1 - t, 2) * p0 + 2 * (1 - t) * t * p1 + Mathf.Pow(t, 2) * p2;
+        Destroy(arrow);
     }
 
 
@@ -89,11 +58,5 @@ public class Archer : MonoBehaviour
     {
         Debug.Log("Shooting at " + enemy.name);
         enemy.GetComponent<Health>().TakeDamage((int)damage);
-    }
-
-    GameObject[] GetEnemiesInRange()
-    {
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, range);
-        return hitColliders.Where(c => c.CompareTag("Enemy")).Select(c => c.gameObject).ToArray();
     }
 }
---
+++ Assets/Scripts/Towers/TowerHelper.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+public static class TowerHelpers
+{
+    public static class TowerProjectileRotationTypes
+    {
+        public const string NONE = "none";
+        public const string LOOK_AT_TARGET = "lookAtTarget";
+        public const string SPIN = "spin";
+    }
+
+    public static GameObject[] GetEnemiesInRange(Transform transform, float range)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, range);
+        return hitColliders.Where(c => c.CompareTag("Enemy")).Select(c => c.gameObject).ToArray();
+    }
+
+    public static Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        return Mathf.Pow(1 - t, 2) * p0 + 2 * (1 - t) * t * p1 + Mathf.Pow(t, 2) * p2;
+    }
+
+    public static IEnumerator AnimateBezierProjectile(GameObject projectile, Vector3 startPosition, GameObject target, float height, float duration, string rotationType = TowerProjectileRotationTypes.NONE)
+    {
+        Vector3 targetPosition = target.transform.position;
+        Vector3 controlPoint = (startPosition + targetPosition) / 2 + Vector3.up * height;
+
+        float startTime = Time.time;
+
+        while (Time.time < startTime + duration)
+        {
+            if (target == null)
+            {
+                // TODO: do not destroy arrows mid air
+                Archer.KillArrow(projectile);
+                yield break;
+            }
+
+            targetPosition = target.transform.position;
+            controlPoint = (startPosition + targetPosition) / 2 + Vector3.up * 2;
+
+            float t = (Time.time - startTime) / duration;
+            Vector3 currentPosition = CalculateBezierPoint(t, startPosition, controlPoint, targetPosition);
+            projectile.transform.position = currentPosition;
+
+            Vector3 direction = (CalculateBezierPoint(t + 0.01f, startPosition, controlPoint, targetPosition) - currentPosition).normalized;
+            projectile.transform.rotation = Quaternion.LookRotation(direction) * Quaternion.Euler(90, 0, 0);
+
+            yield return null;
+        }
+
+        Archer.KillArrow(projectile);
+    }
+}
