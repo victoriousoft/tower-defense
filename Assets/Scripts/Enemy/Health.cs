b95418b614a4b4848213a3b5867b7e0b453df3eb
ViktorJakove <viktor.jakovec@student.gyarab.cz>
2024-10-10 22:51:24 +0200

    damage receiving code imporvement

--- Assets/Scripts/Enemy/Health.cs
+++ Assets/Scripts/Enemy/Health.cs
@@ -8,7 +8,7 @@ public class Health : MonoBehaviour
     public int cashDrop;
     [Range(0, 4)] public int physicalResistance;
     [Range(0, 4)] public int magicResistance;
-    private float[] resistanceValues = new float[] { 1, 0.5f, 0.35f, 0.2f, 0 };//res lvl 1- 50%, lvl2-65%, lvl3-80%
+    private float[] resistanceValues = new float[] { 1, 0.5f, 0.35f, 0.2f, 0 };
 
     private HealthBar healthBar;
     private PlayerStatsManager playerStats;
@@ -22,9 +22,9 @@ public class Health : MonoBehaviour
 
     public void TakeDamage(float damage, int damageType)
     {
-        health -= (damageType == DamageTypes.PHYSICAL && physicalResistance > 0) ? damage * resistanceValues[physicalResistance] :
-             (damageType == DamageTypes.MAGIC && magicResistance > 0) ? damage * resistanceValues[magicResistance] :
-             (damageType == DamageTypes.EXPLOSION && physicalResistance > 0) ? damage * (resistanceValues[physicalResistance] / 2) : damage;
+        health -= (damageType == DamageTypes.PHYSICAL) ? damage * resistanceValues[physicalResistance] :
+             (damageType == DamageTypes.MAGIC) ? damage * resistanceValues[magicResistance] :
+             (damageType == DamageTypes.EXPLOSION) ? damage * (resistanceValues[physicalResistance] / 2) : damage;
 
         healthBar.SetHealth(health / maxHealth);
 
