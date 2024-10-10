4422c675981f43a7062ad9d322ca2714462ddd8d
ViktorJakove <viktor.jakovec@student.gyarab.cz>
2024-10-10 22:41:39 +0200

    fixed armor not blocking damage

--- Assets/Scripts/Enemy/Health.cs
+++ Assets/Scripts/Enemy/Health.cs
@@ -8,12 +8,10 @@ public class Health : MonoBehaviour
     public int cashDrop;
     [Range(0, 4)] public int physicalResistance;
     [Range(0, 4)] public int magicResistance;
-    private float[] resistanceValues = new float[] { 1, 0.5f, 0.35f, 0.2f, 0 };
+    private float[] resistanceValues = new float[] { 1, 0.5f, 0.35f, 0.2f, 0 };//res lvl 1- 50%, lvl2-65%, lvl3-80%
 
     private HealthBar healthBar;
     private PlayerStatsManager playerStats;
-
-
 
     void Awake()
     {
@@ -22,13 +20,11 @@ public class Health : MonoBehaviour
         playerStats = GameObject.Find("PlayerStats").GetComponent<PlayerStatsManager>();
     }
 
-    //res lvl 1- 50%, lvl2-65%, lvl3-80%
     public void TakeDamage(float damage, int damageType)
     {
         health -= (damageType == DamageTypes.PHYSICAL && physicalResistance > 0) ? damage * resistanceValues[physicalResistance] :
              (damageType == DamageTypes.MAGIC && magicResistance > 0) ? damage * resistanceValues[magicResistance] :
              (damageType == DamageTypes.EXPLOSION && physicalResistance > 0) ? damage * (resistanceValues[physicalResistance] / 2) : damage;
-        health -= damage;
 
         healthBar.SetHealth(health / maxHealth);
 
