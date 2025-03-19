95147f120099412fcff34b50d214638f5e66d132
Kristián Kunc <kristian.kunc@gmail.com>
2025-03-19 15:35:29 +0100

    fix annoying overlay!

--- Assets/Scripts/UI/Overlay/FullscreenOverlayManager.cs
+++ Assets/Scripts/UI/Overlay/FullscreenOverlayManager.cs
@@ -21,10 +21,18 @@ public class FullscreenOverlayManager : MonoBehaviour
 		instance.titleText.text = title;
 		instance.descriptionText.text = description;
 		instance.gameObject.SetActive(true);
+		foreach (Transform child in instance.transform)
+		{
+			child.gameObject.SetActive(true);
+		}
 	}
 
 	public static void Hide()
 	{
 		instance.gameObject.SetActive(false);
+		foreach (Transform child in instance.transform)
+		{
+			child.gameObject.SetActive(false);
+		}
 	}
 }
