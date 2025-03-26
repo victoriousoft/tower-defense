b319f9b00a0272e175825e2f531dbdcc0b9922d2
ViktorJakove <viktor.jakovec@student.gyarab.cz>
2025-03-26 15:58:15 +0100

    basic vietcong functionality (no model)

---
+++ Assets/Scripts/Towers/Archer/Vietcong.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vietcong : BaseEvolutionTower
+{
+	private Animator flipAnimator;
+
+	[SerializeField]
+	private Transform originLeft,
+		originRight;
+	private Transform currentOrigin;
+
+	[SerializeField]
+	private SpriteRenderer spriteRendererLeft,
+		spriteRendererRight;
+
+	private bool facingLeft = true;
+
+	protected override void Start()
+	{
+		base.Start();
+		flipAnimator = GetComponent<Animator>();
+		currentOrigin = originLeft;
+	}
+
+	protected override IEnumerator Shoot(GameObject enemy)
+	{
+		if (enemy != null)
+		{
+			if (facingLeft && enemy.transform.position.x > transform.position.x)
+			{
+				spriteRendererLeft.enabled = false;
+				spriteRendererRight.enabled = true;
+				currentOrigin = originRight;
+				facingLeft = false;
+			}
+			else if (!facingLeft && enemy.transform.position.x < transform.position.x)
+			{
+				spriteRendererLeft.enabled = true;
+				spriteRendererRight.enabled = false;
+				currentOrigin = originLeft;
+				facingLeft = true;
+			}
+		}
+		enemy.GetComponent<BaseEnemy>().TakeDamage(towerData.evolutions[0].damage, DamageTypes.PHYSICAL);
+		yield return null;
+	}
+
+	protected override IEnumerator ChargeUp(GameObject enemy)
+	{
+		yield return null;
+	}
+
+	protected override void KillProjectile(GameObject projectile, GameObject enemy, Vector3 enemyPosition) { }
+}
