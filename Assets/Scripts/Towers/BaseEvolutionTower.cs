895ba14fac3b9459f58bd37e772b49f49994b351
ViktorJakove <viktor.jakovec@student.gyarab.cz>
2025-03-27 00:28:49 +0100

    new skill chargebar waiting for revision

--- Assets/Scripts/Towers/BaseEvolutionTower.cs
+++ Assets/Scripts/Towers/BaseEvolutionTower.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class BaseEvolutionTower : BaseTower
 {
@@ -12,6 +13,7 @@ public abstract class BaseEvolutionTower : BaseTower
 	public int evolutionIndex = -1;
 
 	private HealthBar healthBar;
+	private Image circleImage;
 
 	private bool isSkillCharged = false;
 	private Coroutine skillCoroutine;
@@ -19,6 +21,7 @@ public abstract class BaseEvolutionTower : BaseTower
 	protected override void Awake()
 	{
 		base.Awake();
+		circleImage = GetComponentInChildren<Image>();
 	}
 
 	protected override void Start()
@@ -34,10 +37,12 @@ public abstract class BaseEvolutionTower : BaseTower
 	{
 		isSkillCharged = true;
 		skillCoroutine = null;
+		StartCoroutine(BlinkyBlinky());
 	}
 
 	public void UpgradeSkill()
 	{
+		Debug.Log("Upgrading skill");
 		if (skillCoroutine != null)
 			StopCoroutine(skillCoroutine);
 		healthBar.SetHealth(0);
@@ -65,5 +70,24 @@ public abstract class BaseEvolutionTower : BaseTower
 		Debug.Log("base price: " + price);
 		Debug.Log("evo price: " + towerData.evolutions[evolutionIndex].price / 2);
 		return price + towerData.evolutions[evolutionIndex].price / 2;
+	}
+
+	private IEnumerator BlinkyBlinky()
+	{
+		float initAlpha = circleImage.color.a;
+		while (isSkillCharged)
+		{
+			for (float alpha = initAlpha; alpha >= 0; alpha -= 0.025f)
+			{
+				circleImage.color = new Color(circleImage.color.r, circleImage.color.g, circleImage.color.b, alpha);
+				yield return new WaitForSeconds(0.05f);
+			}
+			for (float alpha = 0; alpha <= initAlpha; alpha += 0.025f)
+			{
+				circleImage.color = new Color(circleImage.color.r, circleImage.color.g, circleImage.color.b, alpha);
+				yield return new WaitForSeconds(0.05f);
+			}
+		}
+		circleImage.color = new Color(circleImage.color.r, circleImage.color.g, circleImage.color.b, initAlpha);
 	}
 }
