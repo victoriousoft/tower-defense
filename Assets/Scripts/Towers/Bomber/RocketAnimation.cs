cb9ebc95946cfa5039b5e560463d2d300f2c98aa
ViktorJakove <viktor.jakovec@student.gyarab.cz>
2025-04-01 22:24:10 +0200

    rockets

--- Assets/Scripts/Towers/Bomber/RocketAnimation.cs
+++ Assets/Scripts/Towers/Bomber/RocketAnimation.cs
@@ -6,10 +6,16 @@ public class RocketAnimation : MonoBehaviour
 {
 	public Transform sprite;
 	private Vector3 lastPosition;
+	private ParticleSystem ps;
 
 	void Start()
 	{
 		lastPosition = transform.position;
+		if (GetComponentInChildren<ParticleSystem>() != null)
+		{
+			ps = GetComponentInChildren<ParticleSystem>();
+			ps.Play();
+		}
 	}
 
 	void Update()
@@ -18,7 +24,7 @@ public class RocketAnimation : MonoBehaviour
 
 		if (direction != Vector3.zero)
 		{
-			float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+			float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
 			sprite.rotation = Quaternion.Euler(0, 0, angle);
 		}
 		lastPosition = transform.position;
