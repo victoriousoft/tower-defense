f920382113ad24ae9e7c5b6f9ae80626031a311b
ViktorJakove <viktor.jakovec@student.gyarab.cz>
2025-04-04 21:45:16 +0200

    urza prep

---
+++ Assets/Scripts/Enemy/urza/URZA.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URZA : BaseEnemy
+{
+	void Start() { }
+
+	void Update() { }
+
+	protected override void Attack() { }
+}
