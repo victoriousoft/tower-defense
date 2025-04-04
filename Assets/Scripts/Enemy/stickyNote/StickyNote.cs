06d426dd1f72eb273518e1ea25c0aeaa9dc89568
ViktorJakove <viktor.jakovec@student.gyarab.cz>
2025-04-04 02:02:40 +0200

    sticky note prework

---
+++ Assets/Scripts/Enemy/stickyNote/StickyNote.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyNote : BaseEnemy
+{
+	protected override void Attack()
+	{
+		//move my sprite object towards the currenttarget
+		//kill the target
+		//death anim = pad k zemi
+	}
+
+	protected override void UseAbility() { }
+}
